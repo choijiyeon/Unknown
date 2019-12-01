1ff3bdc2c6281a66f66b20b424e53c296c1b2390
cjy-PC\cjy <jiyeonchoi6617@gmail.com>
2019-12-01 16:23:59 +0900

    배경 이동 작업

---
+++ UnKnown/Assets/Scripts/BackGroundScroll.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScroll : MonoBehaviour
+{
+    public Transform BackGround1;
+    public Transform BackGround2;
+    public Transform cam;
+
+    private bool wichone = true;
+    private float currenth = 15;
+
+	void Update ()
+    {
+		if(currenth < cam.position.x)
+        {
+            if (wichone)
+                BackGround1.localPosition = new Vector3((BackGround1.localPosition.x + 30), -3, 0);
+            else
+                BackGround2.localPosition = new Vector3((BackGround2.localPosition.x + 30), -3, 0);
+
+            currenth += 15;
+
+            wichone = !wichone;
+        }
+        if(currenth > cam.position.x + 15)
+        {
+            if (wichone)
+                BackGround2.localPosition = new Vector3((BackGround2.localPosition.x - 30), -3, 0);
+            else
+                BackGround1.localPosition = new Vector3((BackGround1.localPosition.x - 30), -3, 0);
+
+            currenth -= 15;
+
+            wichone = !wichone;
+        }
+	}
+}
--- UnKnown/Assets/Scripts/PlayerController.cs
+++ UnKnown/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float movePower = 1f;
     public float jumpPower = 1f;
